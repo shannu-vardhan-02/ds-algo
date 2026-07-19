283a02bbc6cd1231ed1cc035644069155a00959f
Shanmukh <shanmukhpedapatruni07@gmail.com>
2026-07-19 23:39:48 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.63%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        // here we are implementing stack using string
+        string st = ""; 
+        vector<int> freq(26, 0);
+        vector<bool> inStack(26, 0);
+
+        for(char c : s){
+            freq[c - 'a']++;
+        }
+        // if a smaller char can appear before a larger one that occurs again later, remove the larger char to obtain to lexicographically smaller subsequence
+        for(char c : s){
+            freq[c - 'a']--;
+            if(inStack[c - 'a']) continue;
+            while(!st.empty() && c < st.back() && freq[st.back() - 'a'] > 0){
+                inStack[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+            st.push_back(c);
+            inStack[c - 'a'] = true;
+        }
+        
+        return st;
+    }
+};

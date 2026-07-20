d2cbdc8b819cffb84912b84cb14b3e424b7939e1
Shanmukh <shanmukhpedapatruni07@gmail.com>
2026-07-20 23:38:35 +0530

    Time: 32 ms (24.43%), Space: 23.2 MB (29.64%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int islands = 0;
+
+        vector<int> dx = {-1, +1, 0, 0};
+        vector<int> dy = {0, 0, -1, +1};
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while(!q.empty()){
+                        auto [cx, cy] = q.front();
+                        q.pop();
+                        for(int k = 0; k < 4; k++){
+                            int nx = cx + dx[k];
+                            int ny = cy + dy[k];
+                            if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == '1'){ 
+                                q.push({nx, ny});
+                                grid[nx][ny] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return islands;
+    }
+};
